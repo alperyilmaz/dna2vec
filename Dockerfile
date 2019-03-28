9dfb3ce55393f2f3431b224e9bab9ca68cb81c4d
Alper Yilmaz <alperyilmaz@gmail.com>
2019-03-28 09:52:12 +0300

    add nworkers and blas

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ubuntu
 
-RUN apt update && apt install -y --no-install-recommends python3-pip python3-setuptools python3-wheel
+RUN apt update && apt install -y --no-install-recommends libopenblas-base libopenblas-dev liblapack-dev python3-pip python3-setuptools python3-wheel python3-numpy python3-scipy python3-nose python3-dev
 
 COPY requirements.txt /
 
--- scripts/train_dna2vec.py
+++ scripts/train_dna2vec.py
@@ -21,7 +21,7 @@ class InvalidArgException(Exception):
     pass
 
 class Learner:
-    def __init__(self, out_fileroot, context_halfsize, gensim_iters, vec_dim):
+    def __init__(self, out_fileroot, context_halfsize, gensim_iters, vec_dim, nworkers):
         self.logger = logbook.Logger(self.__class__.__name__)
         # assert(Word2Vec.FAST_VERSION >= 0)
         # self.logger.info('Word2Vec.FAST_VERSION (should be >= 0): {}'.format(Word2Vec.FAST_VERSION))
@@ -29,6 +29,7 @@ class Learner:
         self.out_fileroot = out_fileroot
         self.context_halfsize = context_halfsize
         self.gensim_iters = gensim_iters
+        self.nworkers = nworkers
         self.use_skipgram = 1
         self.vec_dim = vec_dim
 
@@ -36,6 +37,7 @@ class Learner:
         self.logger.info('Use skipgram: {}'.format(self.use_skipgram))
         self.logger.info('gensim_iters: {}'.format(self.gensim_iters))
         self.logger.info('vec_dim: {}'.format(self.vec_dim))
+        self.logger.info('nworkers: {}'.format(self.nworkers))
 
     def train(self, kmer_seq_generator):
         self.model = Word2Vec(
@@ -43,7 +45,7 @@ class Learner:
             size=self.vec_dim,
             window=self.context_halfsize,
             min_count=5,
-            workers=4,
+            workers=self.nworkers,
             sg=self.use_skipgram,
             iter=self.gensim_iters)
 
@@ -82,7 +84,7 @@ def run_main(args, inputs, out_fileroot):
         histogram,
     )
 
-    learner = Learner(out_fileroot, args.context, args.gensim_iters, args.vec_dim)
+    learner = Learner(out_fileroot, args.context, args.gensim_iters, args.vec_dim,args.nworkers)
     learner.train(kmer_seq_iterable)
     learner.write_vec()
 
@@ -103,6 +105,7 @@ def main():
     argp.add_argument('--context', help='half size of context window (the total size is 2*c+1)', type=int, default=4)
     argp.add_argument('--epochs', help='number of epochs', type=int, default=1)
     argp.add_argument('--gensim-iters', help="gensim's internal iterations", type=int, default=1)
+    argp.add_argument('--nworkers', help="number of workers", type=int, default=4)
     argp.add_argument('--out-dir', help="output directory", default='results')
     argp.add_argument('--debug', help='', action='store_true')
     args = argp.parse_args()
